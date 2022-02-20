d95203625c6a84ab0da67e789c8b2dfd0987fb35
JUANPRASETYA@123 <91299230+juanPrassetya@users.noreply.github.com>
2022-02-20 19:41:38 +0700

    Codingan

---
+++ profile.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juan Prasetya</title>
+    <link href="model.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="content">
+            <div class="logo"><a href="#"></a></div>
+            <ul class="menu-list">
+                <div class="icon cancel-btn">
+                    <i class="fas fa-times"></i>
+                </div>
+                
+                <li><a href="#">My Profile</a></li>
+                <li><a href="tentang.php">About Me</a></li>
+              
+                <li><a href="kontak.php">Contact</a></li>
+                
+            </ul>
+        
+            <div class="icon menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <center><div class="card">
+    <div class="container">
+    <div class="sejarah">
+        <div class="content">
+            <img src="foto gw.jpg" alt="foto">
+                 
+            <h1>-===Juan Prasetya Aditama===-</h1>
+            <p>
+            <div class="title">MY Profile</div>
+            <div class="pencapaian">
+            </p>
+            <p class="sjrh"> 	
+                Assalamualikum warahmatullahi wabarakatuh
+                <br>
+                 Nama saya Juan Prasetya Aditama, saya berumur 15 Tahun, saya 
+                 Jurusan PPLG (Pengembangan Perangkat Lunak dan Gim) Murid kelas 10, Saat ini saya bersekolah di SMK Wikrama Bogor, yang dimana sekolah yang terkenal dengan 
+                kerapihannya, kemajuannya dalam mendidik peserta didiknya, dan Juga sekolah yang sangat berpengruh bagi negara, terutamanya untuk
+                para generasi muda, yang dimana mereka harus memajukan Bangsa Indonesia dengan cara mempertahankan kemajuan Indonesia.
+                <br>
+                Dan Hobby saya adalah mendesign (gambar, web dll )/ menggambar (melukis) & membuat pemograman. dan Cita-cita saya adalah menjadi orang yang 
+                sukses di masa depan dan menjadi anak yang sukses membahagiakan kedua orang tua dengan ingin menjadi ahli programmer dan designer/UI designer,
+                yang dimana UI designer ini bekerja sama dengan  UX Designer, developer.
+                <br>
+
+            </p>
+            
+            
+            </div>
+        
+        </div>
+    </div>
+    </div>
+
+    <!-- <footer class="footer-main">
+        <div class="footer-grid">
+            <section class="footer-1">
+                <h4>Tentang Wikrama</h4>
+                <ul class="links">
+                  <li class="listlinks"><p>sejarah</p></li>
+                  <li class="listlinks"><a href="perpustakaan.html">Perpustakaan Wikrama</a></li>
+                  
+                  <li class="listlinks"><a href="kantin.html">Kantin Wikrama</a></li>
+                </ul>
+              </section>
+             <section class="footer-2">
+                <h4>Alamat</h4>
+                <ul class="links">
+                <li class="listlinks"><p>Jl. Raya Wangun
+                    Kelurahan Sindangsari
+                    Bogor Timur 16720</p></li>
+                </ul>
+                <ul class="links">
+                <h4>Telepon</h4>
+                <li class="listlinks"><p>0251-8242411 / 082221718035 (Whatsapp)</p></li>
+                </ul>
+              </section>
+             <section class="footer-3">
+               <h4>Sosial Media</h4>
+               <ul class="links">  
+                 <li class="listlinks"><i class="fab fa-youtube fa-lg"></i><a href="https://www.youtube.com/channel/UCyhEUzlXbXet57qFnDfdWuw" target="_blank">Multimedia Wikrama</a></li>
+                 <li class="listlinks"><i class="fab fa-instagram fa-lg"></i><a href="https://www.instagram.com/smkwikrama/" target="_blank">smkwikrama</a></li>
+                 <li class="listlinks"><i class="fab fa-twitter-square fa-lg"></i><a href="https://twitter.com/smkwikrama" target="_blank">SMK Wikrama</a></li>
+                 <li class="listlinks"><i class="fab fa-facebook-square fa-lg"></i><a href="https://www.facebook.com/smkwikrama/" target="_blank">SMK Wikrama Bogor</a></li>
+                </ul>
+             </section>
+             <section class="footer-4">
+                
+              </section>
+        </div>
+    </footer> -->
+   
+</div></center>
+    
+</body>
+
+</html>
